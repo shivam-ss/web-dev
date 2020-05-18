12e749ee8ec46b7b94f732597f727f78833f6672
Shivam Srivastava <48158198+shivam-ss@users.noreply.github.com>
2020-05-18 20:47:54 +0530

    Added css in React-Practice

--- src/index.js
+++ src/index.js
@@ -6,10 +6,19 @@ const name = "Shivam";
 const currdate = new Date();
 const year = currdate.getFullYear();
 
-const imag = "https://picsum.photos/200";
+const imag = "https://picsum.photos/160";
+
+//creating a JSON object for
+const customStyle = {
+  color: "green",
+  fontSize: "50px",
+  Border: "1px solid black"
+};
 
 ReactDOM.render(
   <div>
+    <h1 style={{ color: "red" }}> Hiiiiiiii </h1>
+    <h1 style={customStyle}> Sakshi </h1>
     <p>Created by {name} </p>
     <p>Copyright {year} </p>
     <img src={imag} />
